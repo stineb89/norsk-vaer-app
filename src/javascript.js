0da574bba871ee9067df028170948ab63119c48f
Stine <stineberg2@gmail.com>
2025-02-18 16:14:19 +0100

    Added js to time and date function

--- src/javascript.js
+++ src/javascript.js
@@ -1,12 +1,30 @@
-function handleSearch(event) {
-  event.preventDefault();
-  let searchCity = document.querySelector("#searchCity");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchCity.value;
+//date and time
+
+function updateClock() {
+  let local = moment().locale("nb").format("LLLL");
+
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = `I dag er det ${local}`;
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearch);
+updateClock();
 
-let currentCityElement = document.querySelector("#city");
-currentCity.innerHTML = response.data.city;
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Søn", "Man", "Tir", "Ons", "Tor", "Fre", "Lør"];
+
+  return days[date.getDay()];
+
+  // search
+
+  function handleSearch(event) {
+    event.preventDefault();
+    let searchCity = document.querySelector("#searchCity");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchCity.value;
+  }
+
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearch);
+}
+
